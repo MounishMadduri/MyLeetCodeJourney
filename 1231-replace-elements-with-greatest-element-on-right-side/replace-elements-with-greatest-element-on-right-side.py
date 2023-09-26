634a4d395de2fcefd5df7263bbb074fbda58803e
MounishMadduri <86410172+MounishMadduri@users.noreply.github.com>
2023-09-26 15:21:15 +0530

    Time: 617 ms (23.83%) | Memory: 17.9 MB (53.31%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -1,35 +1,18 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
 
-        max_list = [0]*len(arr)
+        # initializing an array to store maximum values from the last index other than -1
+        # [,  ,  ,  ,  , -1]
+        # we are looping from the end, and checking max of max value[j] and max_list[j]
+        
+        max_list = [0]*len(arr) 
         max_list[-1] = -1
-        # max_list[-2] = arr[-1]
-        maxi = 0
+
         j = 0
         for i in range(len(arr)-2,-1,-1):
             j =i+1
             max_list[i] = max(arr[j],max_list[j])
-        # print(max_list)
+
         return max_list
-
-
-
-
-
-
-
-
-
-        # res = []
-        # for i in range(len(arr)-1):
-        #     max_all = 0
-        #     for j in range(i+1,len(arr)):
-        #         if arr[j]>max_all:
-        #             max_all = arr[j]
-        #     res.append(max_all)
-        # res.append(-1)
-
-        # # print(res)
-        # return res
 
         

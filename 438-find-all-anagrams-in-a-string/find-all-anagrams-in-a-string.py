84933a74bee94a15602f7a9b17dd644929d54ddc
MounishMadduri <86410172+MounishMadduri@users.noreply.github.com>
2023-10-09 21:12:05 +0530

    Time: 99 ms (68.90%) | Memory: 17.4 MB (53.00%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        
+        res =[]
+
+        hm ={}
+        for i in range(len(p)):
+            if p[i] not in hm:
+                hm[p[i]] = 0
+            hm[p[i]] += 1
+        
+        hm_res = {}
+        l = 0
+        for i in range(len(s)):
+            if s[i] not in hm_res:
+                hm_res[s[i]] = 0
+            hm_res[s[i]] += 1
+            
+            if i - l + 1 == len(p):
+                if hm_res == hm:
+                    res.append(l)
+                
+                if s[l] in hm_res:
+                    hm_res[s[l]] -= 1
+                    if hm_res[s[l]] == 0:
+                        del hm_res[s[l]]
+                l += 1
+
+        return res
+                

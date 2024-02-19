8372761038dad58a3f16197dfac8c0b1234d3e95
MounishMadduri <86410172+MounishMadduri@users.noreply.github.com>
2024-02-19 23:01:35 +0530

    Time: 534 ms (29.02%) | Memory: 18.9 MB (45.74%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -4,15 +4,27 @@ class Solution:
         # initializing an array to store maximum values from the last index other than -1
         # [,  ,  ,  ,  , -1]
         # we are looping from the end, and checking max of max value[j] and max_list[j]
+
+
+        # I could have just replaced into the same array without creating a new array! lol!
         
-        max_list = [0]*len(arr) 
-        max_list[-1] = -1
+        # max_list = [0]*len(arr) 
+        # max_list[-1] = -1
 
-        j = 0
-        for i in range(len(arr)-2,-1,-1):
-            j =i+1
-            max_list[i] = max(arr[j],max_list[j])
+        # j = 0
+        # for i in range(len(arr)-2,-1,-1):
+        #     j =i+1
+        #     max_list[i] = max(arr[j],max_list[j])
 
-        return max_list
+        # return max_list
+
+        # [17,18,5,4,6,1] [0,0,0,0,0,-1]
+        maxi = [0]*len(arr)
+        maxi[-1] = -1
+        j = len(maxi)-1
+        for i in range(len(maxi)-2,-1,-1):
+            maxi[i] = max(arr[j], maxi[j])
+            j-=1
+        return maxi
 
         

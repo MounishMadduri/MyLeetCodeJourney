1ec97b79ddbd17764b6a49b0e1a7662a7ed32e4f
MounishMadduri <86410172+MounishMadduri@users.noreply.github.com>
2024-02-21 08:31:54 +0530

    Time: 51 ms (22.32%) | Memory: 17 MB (57.43%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -2,26 +2,33 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
         # isomorphic means s should be replacesd with t and t should be replace with s
-
-        hm ={}
-        for i in range(len(s)):
-            if s[i] not in hm :
-                hm[s[i]] = t[i]
-            
+        
         hm1 = {}
-        for i in range(len(t)):
-            if t[i] not in hm1:
-                hm1[t[i]] = s[i]
-            
-        # print(hm)
+        hm2 = {}
+
+        for i in range(len(s)):
+            if s[i] not in hm1:
+                hm1[s[i]] = {t[i]}
+            else:
+                hm1[s[i]].add(t[i])
+        for i in hm1.values():
+            if len(i)>1:
+                return False
         # print(hm1)
-
-        res = ''
+        hm1 = {}
         for i in s:
-            res+=hm[i]
-
-        res1 = ''
+            if i not in hm1:
+                hm1[i] = 0
+            hm1[i]+=1
         for i in t:
-            res1+=hm1[i]
+            if i not in hm2:
+                hm2[i] = 0
+            hm2[i]+=1
 
-        return res == t and res1 == s
+        r1 = ''
+        for i in s:
+            r1+=str(hm1[i])
+        r2 = ''
+        for i in t:
+            r2+=str(hm2[i])
+        return r1==r2

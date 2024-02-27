e448daae5f9dbd339c72f59f742cb4b872732229
MounishMadduri <86410172+MounishMadduri@users.noreply.github.com>
2024-02-27 22:52:54 +0530

    Time: 111 ms (77.24%) | Memory: 17.3 MB (58.05%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,28 +1,23 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        
-        l = 0
-        hm = {'a':0,'e':0,'i':0,'o':0,'u':0}
 
-        sub = s[0:k]
-        for i in sub:
-            if i in hm:
-                hm[i] += 1
-        
-        some = sum(hm.values())
-        maxi = some
-        for r in range(k,len(s)):
-            if s[l] in hm:
-                some-=1
-            if s[r] in hm:
-                some+=1
-                maxi = max(maxi,some)
-            l+=1
-        
+        #hm = {"a": 0, "e": 0, "i": 0, "o": 0, "u": 0}
+        hs = {'a','e','i','o','u'}
+        firstWindow = s[0:k]
+        c = 0
+        maxi = 0
+        for i in firstWindow:
+            if i in hs:
+                c+=1
+        i = 0
+
+        maxi = max(maxi, c)
+        for j in range(k,len(s)):
+            if s[i] in hs:
+                c-=1
+            i+=1
+            if s[j] in hs:
+                c+=1
+            maxi = max(maxi, c)
+        print(maxi)
         return maxi
-
-            
-
-
-            
-

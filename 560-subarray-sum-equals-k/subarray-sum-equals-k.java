9189f23cf05c1878b4679b5b0182dd441e61b12a
MounishMadduri <86410172+MounishMadduri@users.noreply.github.com>
2026-05-30 14:23:13 +0530

    Time: 1553 ms (7.51%) | Memory: 48.5 MB (87.09%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int c= 0;
+        for (int i= 0; i< nums.length; i++) {
+            int currentSum = 0;
+            
+            for (int j= i; j<nums.length; j++) {
+                currentSum += nums[j];
+                
+                if (currentSum == k) {
+                    c++;
+                }
+            }
+        }
+        
+        return c;
+    }
+}
